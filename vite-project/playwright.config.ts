24b4bb6697a580c4f9bc8bb7eef782f18f9d4ffa
clempetre <71828831+clempetre@users.noreply.github.com>
2024-01-18 16:49:08 +0100

    run

--- vite-project/playwright.config.ts
+++ vite-project/playwright.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig, devices } from '@playwright/test';
-
+ 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
+* Read environment variables from file.
+* https://github.com/motdotla/dotenv
+*/
 // require('dotenv').config();
  
 /**
- * See https://playwright.dev/docs/test-configuration.
- */
+* See https://playwright.dev/docs/test-configuration.
+*/
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
@@ -24,29 +24,29 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-     baseURL: 'http://localhost:5173/',
-
+    baseURL: 'http://localhost:5173/',
+ 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
-
+ 
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
+ 
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-
+ 
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
-
+ 
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
@@ -56,7 +56,7 @@ export default defineConfig({
     //   name: 'Mobile Safari',
     //   use: { ...devices['iPhone 12'] },
     // },
-
+ 
     /* Test against branded browsers. */
     // {
     //   name: 'Microsoft Edge',
@@ -67,7 +67,7 @@ export default defineConfig({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
-
+ 
   /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
